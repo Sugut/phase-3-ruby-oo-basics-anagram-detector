a167ebad0c7d1f58987a6583050531766c3f50ed
Sugut <sugutvictorine@gmail.com>
2022-10-20 21:26:34 +0300

    commit tree 894adb91d755eadcf9b23e1284484f4cded71f2f

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    
+    def initialize(word)
+        @word=word
+    end
+
+    def match letters
+        letters.select do |w|
+        w.split("").sort == @word.split("").sort
+    end
+end
+end    
